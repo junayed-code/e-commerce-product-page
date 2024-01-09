eb2d433e3f7bbbfa71b308500d54f7ab9810264d
junayed-code <junayed.code@gmail.com>
2024-01-09 18:10:25 +0600

    Update main layout component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,17 +1,24 @@
-import { Inter } from "next/font/google";
+import CartProvider from "@/providers/CartProvider";
+import Navbar from "@/components/navbar";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Frontend Mentor - E-commerce product page solution",
-  description: "This is a solution to the E-commerce product page challenge on Frontend Mentor.",
+  description:
+    "This is a solution to the E-commerce product page challenge on Frontend Mentor.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="text-gray-800">
+        <div className="lg:max-w-[1088px] mx-auto">
+          <CartProvider>
+            <Navbar />
+            {children}
+          </CartProvider>
+        </div>
+      </body>
     </html>
   );
 }
