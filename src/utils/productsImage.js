0acacca887c2c7e58857f8e2d84e23a7e573f759
junayed-code <junayed.code@gmail.com>
2024-01-09 17:19:13 +0600

    Create file to exporting product images

---
+++ src/utils/productsImage.js
@@ -0,0 +1,31 @@
+import productImg1 from "@/images/image-product-1.jpg";
+import productImg2 from "@/images/image-product-2.jpg";
+import productImg3 from "@/images/image-product-3.jpg";
+import productImg4 from "@/images/image-product-4.jpg";
+import productThum1 from "@/images/image-product-1-thumbnail.jpg";
+import productThum2 from "@/images/image-product-2-thumbnail.jpg";
+import productThum3 from "@/images/image-product-3-thumbnail.jpg";
+import productThum4 from "@/images/image-product-4-thumbnail.jpg";
+
+export default [
+  {
+    id: 1,
+    src: productImg1,
+    thumbSrc: productThum1,
+  },
+  {
+    id: 2,
+    src: productImg2,
+    thumbSrc: productThum2,
+  },
+  {
+    id: 3,
+    src: productImg3,
+    thumbSrc: productThum3,
+  },
+  {
+    id: 4,
+    src: productImg4,
+    thumbSrc: productThum4,
+  },
+];
